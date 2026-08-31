2ace76848113413ca7f6fc98ec047a5bbc2cbac5
Kleiton <kleiton.diogo@outlook.com>
2026-08-31 19:53:19 -0300

    Questão 2

---
+++ Lista 1 - Questão 2.py
@@ -0,0 +1,5 @@
+descalocamento = float(input("Digite o deslocamento (em horas): "))
+segundos = deslocamento * 3600
+distancia = 30 * segundos
+metros = distancia * 1000
+print(f"A distância percorrida é: {metros} metros")
