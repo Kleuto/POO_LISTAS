ba77692fd0a7a4036fdeaa48e07b1fe7c8999274
Kleiton <kleiton.diogo@outlook.com>
2026-08-31 23:19:58 -0300

    Questão 6

---
+++ LISTA 1/Lista 1 - Questão 6.py
@@ -0,0 +1,4 @@
+#tive que usar o robo pra fazer essa professor... mas pelo menos entendi a logica
+expressao = (8 * (121 * 0.5) + 1) * 3
+raiz_cubica = expressao ** (1/3)
+print(f"A raiz cúbica do valor obtido é: {round(raiz_cubica)}")
