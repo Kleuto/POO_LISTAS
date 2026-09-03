81574f7f9a23a25552663bfa193b0a2d45454a8b
Kleiton <kleiton.diogo@outlook.com>
2026-09-02 21:34:43 -0300

    Questão 11

---
+++ LISTA 1/Lista 1 - Questão 11.py
@@ -0,0 +1,11 @@
+#2 questões iguais? tava com preguiça em professor...
+financiamento = float(input("Digite o valor do financiamento: "))
+juros_mensal = float(input("Digite a taxa de juros mensal (em %): "))
+meses = int(input("Digite o número de meses para quitar o financiamento: "))
+
+juros_decimal = juros_mensal / 100
+juros_total = financiamento * juros_decimal * meses
+montante_total = financiamento + juros_total
+
+print(f"O valor dos juros acumulados é: R$ {juros_total:.2f}")
+print(f"O montante total a ser pago é: R$ {montante_total:.2f}")
