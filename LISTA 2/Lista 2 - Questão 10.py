1516589ee914384896e02254c4c607e2e0b9c802
Kleiton <kleiton.diogo@outlook.com>
2026-09-03 23:36:37 -0300

    Questão 10

---
+++ LISTA 2/Lista 2 - Questão 10.py
@@ -0,0 +1,10 @@
+estudante = {
+    "nome": "Kaike",
+    "matricula": "20260001",
+    "idade": 22,
+    "curso": "Licenciatura em Ciência da Computação",
+    "semestre": 7
+}
+
+for chave, valor in estudante.items():
+    print(chave, ":", valor)
