02b466b96b93f975c7046ba5a2a59c130a93285a
Kleiton <kleiton.diogo@outlook.com>
2026-09-03 23:36:38 -0300

    Questão 15

---
+++ LISTA 2/Lista 2 - Questão 15.py
@@ -0,0 +1,10 @@
+disciplina = {
+    "nome_disciplina": "POO",
+    "professor": "Mário",
+    "carga_horaria": 60,
+    "quantidade_alunos": 30,
+    "situacao": "Ativa"
+}
+
+for chave, valor in disciplina.items():
+    print(chave, ":", valor)
