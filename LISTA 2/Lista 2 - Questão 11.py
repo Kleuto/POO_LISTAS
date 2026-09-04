d8c1c326695b584ae608d4c382abbf80f5030ea7
Kleiton <kleiton.diogo@outlook.com>
2026-09-03 23:36:37 -0300

    Questão 11

---
+++ LISTA 2/Lista 2 - Questão 11.py
@@ -0,0 +1,15 @@
+estudante = {
+    "nome": "Diogo",
+    "matricula": "20260002",
+    "idade": 26,
+    "curso": "Licenciatura em Ciência da Computação",
+    "semestre": 7
+}
+
+estudante["semestre"] = 4
+
+estudante["email"] = "diogoalves@ifbaiano.edu.br"
+
+del estudante["idade"]
+
+print(estudante)
